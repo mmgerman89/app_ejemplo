3f0adbbd4e4a704564ae48a62bbd29a878ef55db
German Martinez <mmgerman89@gmail.com>
2014-04-02 18:46:56 -0300

    helper full_title agregado

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
+#encoding: utf-8
 module ApplicationHelper
+  
+  # Devuelve el titulo completo de una pagina
+  def full_title(page_title)
+    base_title = "Germán Martínez"
+    
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+  
 end
